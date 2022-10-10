a013cc2908a8d12c2374254c6b1a081802b3b3da
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-10-10 03:22:05 -0700

    Update style
    - Make less use of flexbox and more use of margins and paddings.
    - Fix responsiveness by setting the max-width for the main element to a
      fixed size (in px) instead of using a relative size (percentage).

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const setRating = function chooseRating() {
 
 const submitRating = function ratingSubmission() {
   mainElement.classList.add("thank-you");
-  mainElement.innerHTML = `<img src="./images/illustration-thank-you.svg" alt="Phone" />
+  mainElement.innerHTML = `<img src="./images/illustration-thank-you.svg" alt="Phone" class="thank-you-img" />
   <div class="chosen-rating">You selected ${rating} out of 5</div>
   <div class="text">
     <h1>Thank you!</h1>
