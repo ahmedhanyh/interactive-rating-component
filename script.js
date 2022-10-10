e377881d5740b06b1b8c6fc436512f08d72aa75e
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-10-10 02:19:52 -0700

    Add functionality to buttons

---
+++ script.js
@@ -0,0 +1,28 @@
+let rating = null;
+const mainElement = document.querySelector("main");
+const ratingButtons = document.querySelectorAll(".rating");
+const submitButton = document.querySelector(".submit-btn");
+
+const setRating = function chooseRating() {
+  ratingButtons.forEach((btn) => btn.classList.remove("chosen"));
+  this.classList.add("chosen");
+  rating = +this.textContent;
+};
+
+const submitRating = function ratingSubmission() {
+  mainElement.classList.add("thank-you");
+  mainElement.innerHTML = `<img src="./images/illustration-thank-you.svg" alt="Phone" />
+  <div class="chosen-rating">You selected ${rating} out of 5</div>
+  <div class="text">
+    <h1>Thank you!</h1>
+    <p>
+    We appreciate you taking the time to give a rating. If you ever need
+    more support, don't hesitate to get in touch!
+    </p>
+    </div>`;
+};
+
+ratingButtons.forEach((btn) => {
+  btn.addEventListener("click", setRating);
+});
+submitButton.addEventListener("click", submitRating);
